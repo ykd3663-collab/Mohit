7f71059100c02f68f0ddec2771750a64b505543b
ykd3663-collab <ykd3663@gmail.com>
2026-04-28 08:57:31 +0530

    Mohit.py

--- Mohit.py
+++ Mohit.py
@@ -3,3 +3,12 @@ import streamlit as st
 st.title("My First Streamlit App")
 #Adding text
 st.write("Hello! Creating a simple web application using streamlit library.")
+
+#TeXt Input
+name=st.text_input("Enter your name :")
+#Number Input
+age=st.number_input("Enter your age :")
+
+#Display a message when button is clicked
+if st.button("Submit")
+st.write("Hello.(name)!Welcome to streamlit.")
